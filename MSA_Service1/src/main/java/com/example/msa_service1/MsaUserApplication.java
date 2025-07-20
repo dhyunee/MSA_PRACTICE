606a747e66f9d7975c75b09fb1a4f1aec2ec357a
songb_ <rlthdqls@gmail.com>
2025-07-20 16:07:47 +0900

    Service_User 설정

---
+++ MSA_Service1/src/main/java/com/example/msa_service1/MsaUserApplication.java
@@ -0,0 +1,15 @@
+package com.example.msa_service1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class MsaUserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsaUserApplication.class, args);
+    }
+
+}
---
+++ MSA_Service1/src/main/java/com/example/msa_service1/user/repository/UserRepository.java
@@ -0,0 +1,6 @@
+//package com.example.msa_service1.user.repository;
+//
+//import org.springframework.data.jpa.repository.JpaRepository;
+//
+//public interface UserRepository extends JpaRepository {
+//}
