5966dea5b788eb4384b166fece93cb9489cb2adf
songb_ <rlthdqls@gmail.com>
2025-08-17 18:56:19 +0900

    Repository 작성

--- MSA_Service1/src/main/java/com/example/msa_service1/user/repository/UserRepository.java
+++ MSA_Service1/src/main/java/com/example/msa_service1/user/repository/UserRepository.java
@@ -1,6 +1,11 @@
-//package com.example.msa_service1.user.repository;
-//
-//import org.springframework.data.jpa.repository.JpaRepository;
-//
-//public interface UserRepository extends JpaRepository {
-//}
+package com.example.msa_service1.user.repository;
+
+import com.example.msa_service1.user.common.code.DeleteYN;
+import com.example.msa_service1.user.entity.UserEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<UserEntity,String> {
+    Optional<UserEntity> findByUserNumber(String userNumber);
+}
